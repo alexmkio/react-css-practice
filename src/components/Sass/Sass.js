bc2d78d231c9054156bbd9a7f7ae75c0e8a19307
Alex Kio <alexandermartelkio@gmail.com>
2022-02-07 13:37:32 -0500

    Add Sass examples

---
+++ src/components/Sass/Sass.js
@@ -0,0 +1,140 @@
+import './Sass.scss';
+
+let exampleOne = `.sass_variables {
+  background-color: $primary-color;
+}`
+
+let exampleTwo = `.sass_nesting {
+  p {
+    background-color: $primary-color;
+  }
+}`
+
+let exampleThree = `a {
+  color: blue;
+
+  &:hover { 
+    color: orange; 
+  }
+}`
+
+let exampleFour = `@import '../../styles/_variables.scss'
+.sass_partial {
+  background-color: $primary-color;
+  color: $blue-light;
+}`
+
+let exampleFive = `@mixin flexy($justify-direction) {
+  display: flex;
+  justify-content: $justify-direction;
+}
+
+.sass_mixin {
+  @include flexy(center);
+}`
+
+let exampleSix = `@function invert($color, $amount: 100%) {
+  $inverse: change-color($color, $hue: hue($color) + 180);
+  @return mix($inverse, $color, $amount);
+}
+
+.sass_function {
+  background-color: invert($primary-color, 60%);
+}`
+
+let exampleSeven = `.sass {
+  border: 1px #f00;
+  background-color: yellow;
+}
+
+.sass-extended {
+  @extend .sass;
+  border-width: 5px;
+  border-style: dashed;
+}`
+
+function Sass() {
+  return (
+    <>
+      <h2>
+        Sass
+      </h2>
+
+      <h3>
+        Using variables
+      </h3>
+      <section>
+        <p className='sass_variables'>$primary-color: lime;</p>
+        <code>
+          {exampleOne}
+        </code>
+      </section>
+
+      <h3>
+        Nesting
+      </h3>
+      <section className='sass_nesting'>
+        <p>$secondary-color: red;</p>
+        <code>
+          {exampleTwo}
+        </code>
+      </section>
+
+      <h3>
+        Nesting & Psuedo-selectors
+      </h3>
+      <section>
+        <a>I am a link</a>
+        <code>
+          {exampleThree}
+        </code>
+      </section>
+
+      <h3>
+        Partials
+      </h3>
+      <section>
+        <p className='sass_partial'>This is using an imported partial file</p>
+        <code>
+          {exampleFour}
+        </code>
+      </section>
+
+      <h3>
+        Mixins
+      </h3>
+      <section>
+        <p className='sass_mixin'>This is using a mixin to dynamically flex the container</p>
+        <code>
+          {exampleFive}
+        </code>
+      </section>
+
+      <h3>
+        Functions
+      </h3>
+      <section>
+        <div className='sass_function'>
+          <p>This is using a function to dynamically inverts color</p>
+        </div>
+        <code>
+          {exampleSix}
+        </code>
+      </section>
+
+      <h3>
+        Extending
+      </h3>
+      <section>
+        <div className='sass-extended'>
+          <p>This classname extends another</p>
+        </div>
+        <code>
+          {exampleSeven}
+        </code>
+      </section>
+    </>
+  );
+}
+
+export default Sass;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import App from './components/App/App';
+import Sass from './components/Sass/Sass';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Sass />
   </React.StrictMode>,
   document.getElementById('root')
 );
