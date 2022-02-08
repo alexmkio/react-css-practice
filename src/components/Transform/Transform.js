6b3ee1509db21d944961f9ae9f25d4cfa763b0dd
Alex Kio <alexandermartelkio@gmail.com>
2022-02-08 13:14:40 -0500

    Add Transform transition examples

--- src/components/Transform/Transform.js
+++ src/components/Transform/Transform.js
@@ -1,7 +1,57 @@
 import './Transform.scss';
 
 function Transform() {
-let example1 = ``
+let example1 = `.Xrotated {
+  transform: rotateX(45deg);
+}
+
+.Yrotated {
+  transform: rotateY(45deg);
+}
+
+.Zrotated {
+  transform: rotateZ(45deg);
+}
+
+.rotate3d {
+  transform: rotate3d(1, 1, 1, 45deg);
+}`
+
+let example2 = `.skew {
+  transform: skew(15deg, 15deg);
+}`
+
+let example3 = `.Xscaled {
+  transform: scaleX(2);
+}
+
+.Yscaled {
+  transform: scaleY(2);
+}
+
+.Zscaled {
+  transform: scaleZ(2);
+}
+
+.scale3d {
+  transform: scale3d(2, 2, 2);
+}`
+
+let example4 = `.Xtranslated {
+  transform: translateX(200px);
+}
+
+.Ytranslated {
+  transform: translateY(200px);
+}
+
+.Ztranslated {
+  transform: translateZ(200px);
+}
+
+.translate3d {
+  transform: translate3d(200px, 200px, 200px);
+}`
 
   return (
     <>
@@ -9,35 +59,44 @@ let example1 = ``
         Transform Functions
       </h2>
 
-      <section>
+      <section className='rotate'>
         <h3>Rotate</h3>
-        <p></p>
+        <div class="Xrotated">X Rotated</div>
+        <div class="Yrotated">Y Rotated</div>
+        <div class="Zrotated">Z Rotated</div>
+        <div class="rotate3d">Rotated 3d</div>
         <code>
           {example1}
         </code>
       </section>
 
-      <section>
+      <section className='rotate'>
         <h3>Skew</h3>
-        <p></p>
+        <div class="skew">Skewed</div>
         <code>
-          {example1}
+          {example2}
         </code>
       </section>
 
-      <section>
+      <section className='rotate'>
         <h3>Scale</h3>
-        <p></p>
+        <div class="Xscaled">X Scaled</div>
+        <div class="Yscaled">Y Scaled</div>
+        <div class="Zscaled">Z Scaled</div>
+        <div class="scale3d">Scaled 3d</div>
         <code>
-          {example1}
+          {example3}
         </code>
       </section>
 
-      <section>
+      <section className='rotate'>
         <h3>Translate</h3>
-        <p></p>
+        <div class="Xtranslated">X Translated</div>
+        <div class="Ytranslated">Y Translated</div>
+        <div class="Ztranslated">Z Translated</div>
+        <div class="translate3d">Translated 3d</div>
         <code>
-          {example1}
+          {example4}
         </code>
       </section>
     </>
