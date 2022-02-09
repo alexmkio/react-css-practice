2e16bc38ab2573bb0506bf830d4c50958fac4297
Alex Kio <alexandermartelkio@gmail.com>
2022-02-09 15:20:04 -0500

    Add counter functions

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,11 +1,64 @@
 import './Counter.scss';
 
 function Counter() {
+let example1 = `ol {
+  counter-reset: listCounter;
+}
+
+li {
+  counter-increment: listCounter;
+}
+
+li::after {
+  content: counter(listCounter, upper-roman);
+}`
+
+let example2 = `li::marker {
+  content:  counters(listCounter, '.') ') ';
+}`
+
   return (
     <>
       <h2 id="counter_functions">
         Counter Functions
       </h2>
+
+      <section>
+        <h3>counter</h3>
+        <ol className='counter'>
+          <li></li>
+          <li></li>
+        </ol>
+        <ol className='counter'>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+        <code>
+          {example1}
+        </code>
+      </section>
+
+      <section>
+        <h3>counters</h3>
+        <ol>
+          <li>
+            <ol>
+              <li></li>
+              <li>
+                <ol>
+                  <li></li>
+                </ol>
+              </li>
+              <li></li>
+            </ol>
+          </li>
+          <li></li>
+        </ol>
+        <code>
+          {example2}
+        </code>
+      </section>
     </>
   );
 }
