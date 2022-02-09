a7ef977736d62d59c820a13897dda879ff0095d2
Alex Kio <alexandermartelkio@gmail.com>
2022-02-09 15:19:34 -0500

    Add Math functions

--- src/components/Math/Math.js
+++ src/components/Math/Math.js
@@ -6,6 +6,32 @@ function Math() {
       <h2 id="math_functions">
         Math Functions
       </h2>
+
+      <section>
+        <h3>calc</h3>
+        <p className='calc1'>I am 1rem</p>
+        <p className='calc2'>I am 1rem + 1 vw</p>
+      </section>
+
+      <section>
+        <h3>clamp</h3>
+        <p className='clamp'>minimum value (1rem), preferred value (2vw), maximum value (3rem)</p>
+      </section>
+
+      <section>
+        <h3>min</h3>
+        <p className='min'>min(2rem, 2em, 2vw, 20px)</p>
+      </section>
+
+      <section>
+        <h3>max</h3>
+        <p className='max'>max(2rem, 2em, 2vw, 20px)</p>
+      </section>
+
+      <section>
+        <h3>abs</h3>
+        <p className='abs'>abs(-20px)</p>
+      </section>
     </>
   );
 }
